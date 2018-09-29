e2d54359248fd2affc579703f324716407acb3d4
James Curtis <mybastion@gmail.com>
2018-09-29 09:06:26 +0100

    [IMP] Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,37 @@ var gulp = require('gulp');
 var inject = require('gulp-inject');
 var xslt = require('gulp-xslt');
 var rename = require('gulp-rename');
+var gulpSequence = require('gulp-sequence')
+var clean = require('gulp-clean');
+
+gulp.task('clean-build', function () {
+	return gulp.src('build', {read:false})
+				.pipe(clean());
+});
+
+gulp.task('clean-dist', function () {
+	return gulp.src('dist', {read:false})
+				.pipe(clean());
+});
 
 gulp.task('inject-roster', function () {
 	gulp.src('./src/command-roster.xsl')
 		.pipe(inject(gulp.src(['./src/card.xsl']), {
-		starttag: '<!-- inject:src/card.xsl -->',
-		relative: true,
-		transform: function (filePath, file) {
+			starttag: '<!-- inject:src/card.xsl -->',
+			relative: true,
+			transform: function (filePath, file) {
 		// return file contents as string
 			return file.contents.toString('utf8')
 		}}))
 		.pipe(inject(gulp.src(['./src/roster.xsl']), {
-		starttag: '<!-- inject:src/roster.xsl -->',
-		relative: true,
-		transform: function (filePath, file) {
+			starttag: '<!-- inject:src/roster.xsl -->',
+			relative: true,
+			transform: function (filePath, file) {
 		// return file contents as string
 			return file.contents.toString('utf8')
 		}}))
-	.pipe(gulp.dest('./dist'));
-})
+		.pipe(gulp.dest('./dist'));
+});
 
 gulp.task('xsl-roster', function() {
     gulp.src('./seekers-roster.xml')
@@ -28,8 +40,20 @@ gulp.task('xsl-roster', function() {
         .pipe(gulp.dest('./build/'));
 });
 
+/*gulp.task("rename-roster", function () {
+	return gulp.src("./build/seekers-roster.xml")
+		.pipe(rename("seekers-roster.html"))
+		.pipe(gulp.dest("./build"));
+});*/
+
 gulp.task('rename-roster', function () {
 	gulp.src('./build/seekers-roster.xml')
-		.pipe(rename('seekers-roster.html'))
+		.pipe(rename(function (path) {
+			path.dirname += '/';
+			path.basename += '';
+			path.extname = '.html';
+		}))
 		.pipe(gulp.dest('./build'));
-});
+})
+
+gulp.task('sequence-roster', gulpSequence('inject-roster', 'xsl-roster', 'rename-roster'))
