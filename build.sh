c2fa690d533d9ab8294b15a8355cf4777da9fa99
James Curtis <mybastion@gmail.com>
2018-10-01 19:59:09 +0100

    [REF] Reformat build script for readability

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-gulp clean-dist && gulp clean-build && gulp inject-campaign && gulp xsl-campaign && gulp rename-campaign && gulp inject-matched-play && gulp xsl-matched-play && gulp rename-matched-play
+gulp clean-dist && \
+gulp clean-build && \
+gulp inject-campaign && \
+gulp xsl-campaign && \
+gulp rename-campaign && \
+gulp inject-matched-play && \
+gulp xsl-matched-play && \
+gulp rename-matched-play
