684b5de6f0673e26f5d54b5586df155c7dc8e510
James Curtis <mybastion@gmail.com>
2019-09-23 19:49:45 +0100

    Add rename and browser sync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
-const { src, dest } = require('gulp')
+const { watch, series, src, dest } = require('gulp')
+const browserSync = require('browser-sync').create()
 const xslt = require('gulp-xslt')
 const rename = require('gulp-rename')
 
-function defaultTask(cb) {
-    cb();
-}
-
-function transform(cb) {
-
-    cb();
-}
-
-exports.default = () => {
+function transform () {
     return src('data/necrons-roster.xml')
         .pipe(xslt('src/roster.xsl', {}))
         .pipe(dest('build/'))
+}
+
+function htmlRename () {
+    return src('build/necrons-roster.xml')
+        .pipe(rename('build/necrons-roster.html'))
+        .pipe(dest('./'))
+}
+
+exports.default = () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    watch('src/roster.xsl', series(transform, htmlRename))
+    watch('build/*.html').on('change', browserSync.reload)
 }
